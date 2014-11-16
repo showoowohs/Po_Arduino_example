190ef17f47d9d1f03c8c4bd456e21956e3d0080a
showoowohs <showoowohs@gmail.com>
2014-11-16 12:15:14 +0800

    將btn 設成input_pullup ,解決燈號不受控

--- 3-3_Po_Button_Ctrl_LED/Po_Button_Ctrl_LED/Po_Button_Ctrl_LED.ino
+++ 3-3_Po_Button_Ctrl_LED/Po_Button_Ctrl_LED/Po_Button_Ctrl_LED.ino
@@ -1,11 +1,11 @@
 #define Po_LED13 13
-#define Button 2
+#define Button 3
 
 void setup(){
   //Initialize serial and wait for port to open:
   Serial.begin(9600); 
   pinMode(Po_LED13, OUTPUT);//設定LED 13為output
-  pinMode(Button, INPUT);//設定btn 2為input
+  pinMode(Button, INPUT_PULLUP);//設定btn 2為input
 }
 void loop(){
   if(digitalRead(Button) == LOW){//判斷btn狀態
