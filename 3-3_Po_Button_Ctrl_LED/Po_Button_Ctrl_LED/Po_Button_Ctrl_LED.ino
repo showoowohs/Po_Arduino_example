71a77f3937694553fa56517624dbedcf30e23d7c
showoowohs <showoowohs@gmail.com>
2014-11-16 11:36:18 +0800

    定義LED13 與btn 2號, 再將LED13設定為output, 最後再將button 2號角位設定成input

--- 3-3_Po_Button_Ctrl_LED/Po_Button_Ctrl_LED/Po_Button_Ctrl_LED.ino
+++ 3-3_Po_Button_Ctrl_LED/Po_Button_Ctrl_LED/Po_Button_Ctrl_LED.ino
@@ -1,7 +1,10 @@
-#define Po_LED13
-#define Button
+#define Po_LED13 13
+#define Button 2
 
 void setup(){
+  pinMode(Po_LED13, OUTPUT);//設定LED 13為output
+  pinMode(Button, INPUT);//設定btn 2為input
 }
 void loop(){
+  
 }
