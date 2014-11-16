b20c6bda5174e5043264648252250e648342970a
showoowohs <showoowohs@gmail.com>
2014-11-16 11:57:19 +0800

    添加判斷式

--- 3-3_Po_Button_Ctrl_LED/Po_Button_Ctrl_LED/Po_Button_Ctrl_LED.ino
+++ 3-3_Po_Button_Ctrl_LED/Po_Button_Ctrl_LED/Po_Button_Ctrl_LED.ino
@@ -8,8 +8,14 @@ void setup(){
   pinMode(Button, INPUT);//設定btn 2為input
 }
 void loop(){
-  if(digitalRead(Button) == LOW){
-  Serial.print("is Low!!\n");
+  if(digitalRead(Button) == LOW){//判斷btn狀態
+    delay(20);
+      if(digitalRead(Button) == LOW){//等待20s 後再判斷btn status
+        Serial.print("is Low!!\n");
+        //digitalWrite(Po_LED13, HIGH);//將LED設定HIGH
+        digitalWrite(Po_LED13, !digitalRead(Po_LED13));//將讀取到的LED數值反向
+        while(digitalRead(Button) == LOW);//只有在Button為LOW的時候迴圈, 不是LOW就跳開
+      }
   }
-  
 }
+
