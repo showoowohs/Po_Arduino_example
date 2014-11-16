392f8d146f2b43f7615e70fa4360f626bd432859
showoowohs <showoowohs@gmail.com>
2014-11-16 12:19:38 +0800

    添加debug msg, 方便以後查找

--- 3-3_Po_Button_Ctrl_LED/Po_Button_Ctrl_LED/Po_Button_Ctrl_LED.ino
+++ 3-3_Po_Button_Ctrl_LED/Po_Button_Ctrl_LED/Po_Button_Ctrl_LED.ino
@@ -11,11 +11,13 @@ void loop(){
   if(digitalRead(Button) == LOW){//判斷btn狀態
     delay(20);
       if(digitalRead(Button) == LOW){//等待20s 後再判斷btn status
-        Serial.print("is Low!!\n");
+        Serial.print("is Low , Button click!!\n");
         //digitalWrite(Po_LED13, HIGH);//將LED設定HIGH
         digitalWrite(Po_LED13, !digitalRead(Po_LED13));//將讀取到的LED數值反向
         while(digitalRead(Button) == LOW);//只有在Button為LOW的時候迴圈, 不是LOW就跳開
       }
+  }else{
+    //Serial.print("is HIGH, not click!!\n");
   }
 }
 
