0f7586772163e3914c9323c2b2755813f647556b
showoowohs <showoowohs@gmail.com>
2014-11-16 11:45:04 +0800

    透過serial 添加debug 訊息

--- 3-3_Po_Button_Ctrl_LED/Po_Button_Ctrl_LED/Po_Button_Ctrl_LED.ino
+++ 3-3_Po_Button_Ctrl_LED/Po_Button_Ctrl_LED/Po_Button_Ctrl_LED.ino
@@ -2,9 +2,14 @@
 #define Button 2
 
 void setup(){
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600); 
   pinMode(Po_LED13, OUTPUT);//設定LED 13為output
   pinMode(Button, INPUT);//設定btn 2為input
 }
 void loop(){
+  if(digitalRead(Button) == LOW){
+  Serial.print("is Low!!\n");
+  }
   
 }
