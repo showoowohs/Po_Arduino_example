95841c1e86bd345333fa0adef24a1f28e5752ba5
showoowohs <showoowohs@gmail.com>
2014-11-22 14:13:14 +0800

    只有有調整電位器(Pot)時才更新log, 不然一直噴log會眼花撩亂

--- 3-5_Po_Potentiometer_via_serial/Po_Potentiometer_via_serial/Po_Potentiometer_via_serial.ino
+++ 3-5_Po_Potentiometer_via_serial/Po_Potentiometer_via_serial/Po_Potentiometer_via_serial.ino
@@ -1,6 +1,7 @@
 #define Pot A1// select analog in A1, can use A0~A5
 
 int Pot_buffer = 0;
+int old_buf = -1;
 
 void setup() { 
  //Initialize serial and wait for port to open:
@@ -9,7 +10,10 @@ void setup() {
 
 void loop() { 
   Pot_buffer = analogRead(Pot);
-  Serial.print("Pot_buffer = ");     
-  Serial.println(Pot_buffer);   
+  if(Pot_buffer != old_buf){//new buf 不等於 old buf的話才顯示
+    old_buf = Pot_buffer;
+    Serial.print("Pot_buffer = ");     
+    Serial.println(Pot_buffer);   
+  }
   delay(500);
 }
