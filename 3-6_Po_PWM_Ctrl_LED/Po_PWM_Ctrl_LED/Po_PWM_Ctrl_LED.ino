10b838bb3b3a0096dfab47aff061fdff4eae54c1
showoowohs <showoowohs@gmail.com>
2014-11-22 15:25:32 +0800

    1. 調整Pot 2. 將Pot數值透過map()轉換成0~255的數值 3. 再將此數值丟給analogWrite() 最後就能透過Pot調整亮度了

--- 3-6_Po_PWM_Ctrl_LED/Po_PWM_Ctrl_LED/Po_PWM_Ctrl_LED.ino
+++ 3-6_Po_PWM_Ctrl_LED/Po_PWM_Ctrl_LED/Po_PWM_Ctrl_LED.ino
@@ -1,19 +1,34 @@
 #define Pot A1// select analog in A1, can use A0~A5
+#define LED 3
 
 int Pot_buffer = 0;
-int old_buf = -1;
 
 void setup() { 
  //Initialize serial and wait for port to open:
   Serial.begin(9600);   
+  pinMode(LED, OUTPUT);
 } 
 
 void loop() { 
-  Pot_buffer = analogRead(Pot);
-  if(Pot_buffer != old_buf){//new buf 不等於 old buf的話才顯示
-    old_buf = Pot_buffer;
-    Serial.print("Pot_buffer = ");     
-    Serial.println(Pot_buffer);   
-  }
+  Pot_buffer = analogRead(Pot);//最大值是1023
+  Serial.print("Pot_buffer = ");     
+  Serial.println(Pot_buffer);   
+  
+  //透過map()轉換
+  Pot_buffer =map(Pot_buffer, 0, 1023, 0, 255);//將Pot的數值轉換城0~255, Pot的值本來是0~1023, 現在只會有0~255
+  Serial.print("map(Pot_buffer) = ");     
+  Serial.println(Pot_buffer); 
+  
+  //將0~255的值填入PWM
+  analogWrite(LED, Pot_buffer);
+  //Po_debug();
   delay(500);
+}
+
+void Po_debug(){
+   for(int i =0; i <= 255; i++){
+    Serial.println(i);
+    analogWrite(LED, i);   
+    delay(200);
+  }
 }
