9c9e7932d71295bda053818c8a6f84a847ff9a99
showoowohs <showoowohs@gmail.com>
2014-11-23 14:26:09 +0800

    設置analgo A1 讀取Pot, 在設置3號pin為buzzer輸出, 最後將Pot的數值送給Buzzer, 由於此Buzzer為無緣的所以需要調整Hz, 因此透過for()控制 Frequency, 範例為100

--- 3-7_Po_PWM_Ctrl_Buzzer/Po_PWM_Ctrl_Buzzer/Po_PWM_Ctrl_Buzzer.ino
+++ 3-7_Po_PWM_Ctrl_Buzzer/Po_PWM_Ctrl_Buzzer/Po_PWM_Ctrl_Buzzer.ino
@@ -1,12 +1,12 @@
 #define Pot A1// select analog in A1, can use A0~A5
-#define LED 3
+#define Buzzer 3
 
 int Pot_buffer = 0;
 
 void setup() { 
  //Initialize serial and wait for port to open:
   Serial.begin(9600);   
-  pinMode(LED, OUTPUT);
+  pinMode(Buzzer, OUTPUT);
 } 
 
 void loop() { 
@@ -14,21 +14,22 @@ void loop() {
   Serial.print("Pot_buffer = ");     
   Serial.println(Pot_buffer);   
   
-  //透過map()轉換
-  Pot_buffer =map(Pot_buffer, 0, 1023, 0, 255);//將Pot的數值轉換城0~255, Pot的值本來是0~1023, 現在只會有0~255
-  Serial.print("map(Pot_buffer) = ");     
-  Serial.println(Pot_buffer); 
+  for(int i = 0; i < 100; i++){//1超小聲 10小聲 100剛好 1000尖銳
+    digitalWrite(Buzzer, HIGH); 
+    delayMicroseconds(Pot_buffer);
+    digitalWrite(Buzzer, LOW); 
+    delayMicroseconds(100);
+  }
   
-  //將0~255的值填入PWM
-  analogWrite(LED, Pot_buffer);
   //Po_debug();
-  delay(500);
+
+  delay(1000);
 }
 
 void Po_debug(){
    for(int i =0; i <= 255; i++){
     Serial.println(i);
-    analogWrite(LED, i);   
+
     delay(200);
   }
 }
