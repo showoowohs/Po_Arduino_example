541049dc505dc53563341247bc8d5852d0ff7624
showoowohs <showoowohs@gmail.com>
2014-12-03 21:11:47 +0800

    透過判斷A2的數值來決定LED13亮燈的狀態, 如果CdS讀到大於800 LED13 set LOW, if Cds < 800 LED13 on, if CdS < 700 LED13閃爍800ms

--- 3-8_Po_Read_CdS/Po_Read_CdS/Po_Read_CdS.ino
+++ 3-8_Po_Read_CdS/Po_Read_CdS/Po_Read_CdS.ino
@@ -1,35 +1,44 @@
-#define Pot A1// select analog in A1, can use A0~A5
-#define Buzzer 3
+#define ADpin A2// select analog in A2, can use A0~A5
+#define LED 13
 
-int Pot_buffer = 0;
 
 void setup() { 
  //Initialize serial and wait for port to open:
   Serial.begin(9600);   
-  pinMode(Buzzer, OUTPUT);
+  pinMode(LED, OUTPUT);
 } 
 
 void loop() { 
-  Pot_buffer = analogRead(Pot);//最大值是1023
-  Serial.print("Pot_buffer = ");     
-  Serial.println(Pot_buffer);   
+  int ADpin_buffer =0;
+  ADpin_buffer = analogRead(ADpin);//0~1023
+  Serial.print("ADpin_buffer = ");     
+  Serial.println(ADpin_buffer);   
   
-  for(int i = 0; i < 100; i++){//1超小聲 10小聲 100剛好 1000尖銳
-    digitalWrite(Buzzer, HIGH); 
-    delayMicroseconds(Pot_buffer);
-    digitalWrite(Buzzer, LOW); 
-    delayMicroseconds(100);
-  }
-  
-  //Po_debug();
+  Po_check_CdS(ADpin_buffer);
 
-  delay(1000);
+  delay(500);
 }
 
-void Po_debug(){
-   for(int i =0; i <= 255; i++){
-    Serial.println(i);
+void Po_check_CdS(int CdS_val){
+  int val = CdS_val;
+  if(val > 800){
+    digitalWrite(LED, LOW);
+    Serial.println("CdS_val > 800"); 
+  }else{
+    
+    if(val < 700){
+      digitalWrite(LED, LOW);
 
-    delay(200);
+      delay(800);
+      digitalWrite(LED, HIGH);
+      delay(800);
+      digitalWrite(LED, LOW);
+      delay(800);
+      digitalWrite(LED, HIGH);
+      Serial.println("CdS_val < 700"); 
+    }else{
+      digitalWrite(LED, HIGH);
+      Serial.println("CdS_val < 800"); 
+    }
   }
 }
