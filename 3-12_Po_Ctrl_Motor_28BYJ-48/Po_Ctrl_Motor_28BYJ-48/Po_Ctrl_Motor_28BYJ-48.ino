7547b59cc0735927e5e3d30fb63ddf2e21618411
showoowohs <showoowohs@gmail.com>
2014-12-06 15:21:25 +0800

    參考http://4tronix.co.uk/arduino/Stepper-Motors.php ,可以讓步進馬達work, 原理理解中...

--- 3-12_Po_Ctrl_Motor_28BYJ-48/Po_Ctrl_Motor_28BYJ-48/Po_Ctrl_Motor_28BYJ-48.ino
+++ 3-12_Po_Ctrl_Motor_28BYJ-48/Po_Ctrl_Motor_28BYJ-48/Po_Ctrl_Motor_28BYJ-48.ino
@@ -1,43 +1,61 @@
+int motorPin1 = 2;    
+int motorPin2 = 3;    
+int motorPin3 = 4;    
+int motorPin4 = 5;    
 
+int motorSpeed = 1200;  //variable to set stepper speed
+int count = 0;          // count of steps made
+int countsperrev = 512; // number of steps per full revolution
+int lookup[8] = {B01000, B01100, B00100, B00110, B00010, B00011, B00001, B01001};
 
-void setup() { 
- //Initialize serial and wait for port to open:
-  Serial.begin(9600);   
-
-  pinMode(2, OUTPUT); 
-
-  pinMode(3, OUTPUT);
-
-  pinMode(4, OUTPUT);
-
-  pinMode(5, OUTPUT);
-
-  pinMode(6, OUTPUT);
-
-  pinMode(7, OUTPUT);
-
-  pinMode(8, OUTPUT);
-
-  pinMode(9, OUTPUT);
-
-
-} 
-
-void loop() { 
- digitalWrite(2, 1);
-
-  digitalWrite(3, 1);
-
-  
-  delay(1000);
-  //Serial.print("ADpin_buffer = ");     
-  Serial.println("...");   
-  
-  digitalWrite(2, 0);
-
-  digitalWrite(3, 0);
-  delay(1000);
-  //delay(500);
+//////////////////////////////////////////////////////////////////////////////
+void setup() {
+  //declare the motor pins as outputs
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
+  Serial.begin(9600);
 }
 
+//////////////////////////////////////////////////////////////////////////////
+void loop(){
+  if(count < countsperrev )
+    clockwise();
+  else if (count == countsperrev * 2)
+    count = 0;
+  else
+    anticlockwise();
+  count++;
+  Serial.print("count = ");
+  Serial.println(count);
+}
 
+//////////////////////////////////////////////////////////////////////////////
+//set pins to ULN2003 high in sequence from 1 to 4
+//delay "motorSpeed" between each pin setting (to determine speed)
+void anticlockwise()
+{
+  for(int i = 0; i < 8; i++)
+  {
+    setOutput(i);
+    delayMicroseconds(motorSpeed);
+  }
+}
+
+void clockwise()
+{
+  for(int i = 7; i >= 0; i--)
+  {
+    setOutput(i);
+    delayMicroseconds(motorSpeed);
+  }
+}
+
+void setOutput(int out)
+{
+  digitalWrite(motorPin1, bitRead(lookup[out], 0));
+  digitalWrite(motorPin2, bitRead(lookup[out], 1));
+  digitalWrite(motorPin3, bitRead(lookup[out], 2));
+  digitalWrite(motorPin4, bitRead(lookup[out], 3));
+}
