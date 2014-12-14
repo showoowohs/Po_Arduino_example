3fb4f2883d2731785c90c77485cb20c36323f286
showoowohs <showoowohs@gmail.com>
2014-12-14 15:56:33 +0800

    increase interrupt function, 我的sensor好像不能work, 我也不知道我插上麵包版的元件是否是vibration sensor

--- 3-15_Po_VibrationSensor_ctrl_Buzzer/Po_VibrationSensor_ctrl_Buzzer/Po_VibrationSensor_ctrl_Buzzer.ino
+++ 3-15_Po_VibrationSensor_ctrl_Buzzer/Po_VibrationSensor_ctrl_Buzzer/Po_VibrationSensor_ctrl_Buzzer.ino
@@ -1,20 +1,27 @@
 #define Buzzer 3
+#define Vibration_sensor 2
 
 
 void setup() {
   //init
   Serial.begin(9600);
   pinMode(Buzzer, OUTPUT);
+  pinMode(Vibration_sensor, INPUT_PULLUP);
+  attachInterrupt(0, start_voice, FALLING);
 }
 
 void loop(){
   Serial.println(" C");
+  
+}
+
+void start_voice(){
+  Serial.println("start_voice()");
   digitalWrite(Buzzer, HIGH);
   delay(2000);
   digitalWrite(Buzzer, LOW);
   delay(1000);
 }
-
 
 
 
