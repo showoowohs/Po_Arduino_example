19f3fd3f6b100e7d05af3d4277ddffdd3300fb4e
showoowohs <showoowohs@gmail.com>
2014-12-14 17:34:58 +0800

    modify code, if flame_sensor value > 0 start buzzer

--- 3-16_Po_FlameSensor_ctrl_Buzzer/Po_FlameSensor_ctrl_Buzzer/Po_FlameSensor_ctrl_Buzzer.ino
+++ 3-16_Po_FlameSensor_ctrl_Buzzer/Po_FlameSensor_ctrl_Buzzer/Po_FlameSensor_ctrl_Buzzer.ino
@@ -1,26 +1,35 @@
 #define Buzzer 3
-#define Vibration_sensor 2
-
+#define flame_sensor A5
 
 void setup() {
   //init
   Serial.begin(9600);
   pinMode(Buzzer, OUTPUT);
-  pinMode(Vibration_sensor, INPUT_PULLUP);
-  attachInterrupt(0, start_voice, FALLING);
-}
-
-void loop(){
-  Serial.println(" C");
+  
   
 }
 
-void start_voice(){
-  Serial.println("start_voice()");
+void loop(){
+  int flame_sensor_buf = 0;
+  flame_sensor_buf = analogRead(flame_sensor);
+  Serial.print("flame_sensor_buf = ");
+  Serial.println(flame_sensor_buf);
+  if (flame_sensor_buf > 0){
+    start_buzzer();
+  }else{
+    stop_buzzer();
+  }
+  delay(100);  
+}
+
+void start_buzzer(){
+  Serial.println("start_buzzer()");
   digitalWrite(Buzzer, HIGH);
-  delay(2000);
+}
+
+void stop_buzzer(){
+  Serial.println("stop_buzzer()");
   digitalWrite(Buzzer, LOW);
-  delay(1000);
 }
 
 
