16b3b91dac27785e4f47bbd947f675eb0047b4fc
showoowohs <showoowohs@gmail.com>
2014-12-17 21:54:37 +0800

    參考http://coopermaa2nd.blogspot.tw/2010/12/arduino-lab11-74hc595-8-led.html, 這線路實在很難接, 不知道哪兒沒接好亮的順序不太對

--- 3-20_Po_74HC595_ctrl_8LED/Po_74HC595_ctrl_8LED/Po_74HC595_ctrl_8LED.ino
+++ 3-20_Po_74HC595_ctrl_8LED/Po_74HC595_ctrl_8LED/Po_74HC595_ctrl_8LED.ino
@@ -1,34 +1,32 @@
-//*******************************************************************************
-//----------------本例程仅供学习使用，未经作者允许，不得用于其他用途。-----------
-//------------------------版权所有，仿冒必究！-----------------------------------
-//----------------1.开发环境:Arduino IDE-----------------------------------------
-//----------------2.测试使用开发板型号：Arduino Leonardo or Arduino UNO R3-------
-//----------------3.单片机使用晶振：16M------------------------------------------
-//----------------4.淘宝网址：Ilovemcu.taobao.com--------------------------------
-//----------------------------52dpj.taobao.com-----------------------------------
-//----------------------------epic-mcu.taobao.com--------------------------------
-//----------------5.作者：神秘藏宝室---------------------------------------------
-//*******************************************************************************/
+// Lab11 使用 74HC595 和三支腳位控制 8 顆 LED
+
+// 接 74HC595 的 ST_CP (pin 12,latch pin)
 int latchPin = 8;
+// 接 74HC595 的 SH_CP (pin 11, clock pin)
 int clockPin = 12;
-int dataPin = 11; //这里定义了那三个脚
+// 接 74HC595 的 DS (pin 14)
+int dataPin = 11;
 
-unsigned char table[] = {0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};	//LED状态显示的变量
-
-void setup ()
-{
-  pinMode(latchPin,OUTPUT);
-  pinMode(clockPin,OUTPUT);
-  pinMode(dataPin,OUTPUT); //让三个脚都是输出状态
+void setup() {
+  // 將 latchPin, clockPin, dataPin 設置為輸出
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
 }
-void loop()
-{
-  for(int i=0; i<8; i++)
-  {
-    digitalWrite(latchPin,LOW); //将ST_CP口上面加低电平让芯片准备好接收数据
-    shiftOut(dataPin,clockPin,MSBFIRST,table[i]);
-    														//串行数据输入引脚为dataPin，时钟引脚为clockPin，执行MSB有限发送，发送数据table[i]
-    digitalWrite(latchPin,HIGH); //将ST_CP这个针脚恢复到高电平
-    delay(500);				//延时500ms 
+
+void loop() {
+  // 在 8 顆 LED 上計數數字，從 0 計數到 255
+  for (int numberToDisplay = 0; numberToDisplay < 256; numberToDisplay++) {
+    // 送資料前要先把 latchPin 拉成低電位
+    digitalWrite(latchPin, LOW);
+    
+    // 使用 shiftOut 函式送出資料
+    shiftOut(dataPin, clockPin, MSBFIRST, numberToDisplay);  
+
+    // 送完資料後要把 latchPin 拉回成高電位
+    digitalWrite(latchPin, HIGH);
+    
+    // 隔 500ms 後換下一個數字
+    delay(500);
   }
 }
