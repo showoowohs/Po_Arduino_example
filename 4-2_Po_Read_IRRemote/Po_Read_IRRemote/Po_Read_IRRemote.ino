f716fa82debdfe5a31240bd6ce358224966e18e1
showoowohs <showoowohs@gmail.com>
2015-01-31 16:10:09 +0800

    1. 將library copy 出來 2.將library file rename, fix IDE會報錯 3.將.cpp中的include位置指向這裡的library 4. .ino也要include這個library , reference:http://lamb-mei.com/436/%E4%BD%BF%E7%94%A8-arduino-1-5-x-ide-%E7%B7%A8%E8%AD%AF%E6%9C%89%E5%BC%95%E7%94%A8-irremote-%E5%87%BA%E7%8F%BE-error-tkd2-was-not-declared-in-this-scope/ , solve TDK2 error

--- 4-2_Po_Read_IRRemote/Po_Read_IRRemote/Po_Read_IRRemote.ino
+++ 4-2_Po_Read_IRRemote/Po_Read_IRRemote/Po_Read_IRRemote.ino
@@ -1,120 +1,28 @@
+#include "AIRremote.h"
 
-#include "InfraredRemote.h"
+int RECV_PIN = 2; // 使用數位腳位2接收紅外線訊號
+IRrecv irrecv(RECV_PIN); // 初始化紅外線訊號輸入
+decode_results results; // 儲存訊號的結構
 
-void setup() 
-{ 
-	Serial.begin(9600); 
-	pinMode(IR_IN,INPUT_PULLUP);//设置红外接收引脚为输入
-	Serial.flush();	//清除串口缓冲器内容函数。 
-	timer1_init();//定时器初始化
-} 
-void loop() 
-{ 	
-	remote_decode(); //译码
-	Deal_Print();	
-} 
-
-void Deal_Print()
+void setup()
 {
-	if( adrL_code == 0x45 )
-	{
-		//Serial.println("ON/OFF");			
-                Serial.println("CH-");
-	}
-	else if( adrL_code == 0x46)
-	{
-		Serial.println("CH");			
-	}
-	else if( adrL_code == 0x47 )
-	{
-		//Serial.println("MENU");			
-                Serial.println("CH+");
-	}
-	else if( adrL_code == 0x44 )
-	{
-		//Serial.println("TEST");			
-                Serial.println("Left");
-	}
-	else if( adrL_code == 0x40 )
-	{
-		//Serial.println("+");			
-                Serial.println("Right");
-	}
-	else if( adrL_code == 0x43 )
-	{
-		//Serial.println("Return");
-                Serial.println("Play/Pasue");			
-	}
-	else if( adrL_code == 0x07 )
-	{
-		//Serial.println("Left");			
-                Serial.println("-");
-	}
-	else if( adrL_code == 0x15)
-	{
-		//Serial.println("Play");			
-                Serial.println("+");
-	}
-	else if( adrL_code == 0x09 )
-	{
-		//Serial.println("Right");			
-                Serial.println("EQ");
-	}
-	else if( adrL_code == 0x16)
-	{
-		Serial.println("0");			
-	}
-	else if( adrL_code == 0x19)
-	{
-		//Serial.println("-");			
-                Serial.println("100+");
-	}
-	else if( adrL_code == 0x0d)
-	{
-		//Serial.println("C");			
-                Serial.println("200+");
-	}
-	else if( adrL_code == 0x0c)
-	{
-		Serial.println("1");			
-	}
-	else if( adrL_code == 0x18 )
-	{
-		Serial.println("2");			
-	}
-	else if( adrL_code == 0x5e )
-	{
-		Serial.println("3");			
-	}
-	else if(adrL_code == 0x08 )
-	{
-		Serial.println("4");			
-	}
-	else if( adrL_code == 0x1c )
-	{
-		Serial.println("5");			
-	}
-	else if( adrL_code == 0x5a )
-	{
-		Serial.println("6");			
-	}
-	else if( adrL_code == 0x42)
-	{
-		Serial.println("7");			
-	}
-	else if( adrL_code == 0x52)
-	{
-		Serial.println("8");			
-	}
-	else if( adrL_code == 0x4a)
-	{
-		Serial.println("9");			
-	}else{
-                Serial.println("other");
-        }
-
-	adrL_code = 0x00;
-        adrH_code = 0x00;
-        ir_code = 0x00;
+  /*
+  Serial.begin(115200);
+  irrecv.blink13(true); // 設為true的話，當收到訊號時，腳位13的LED便會閃爍
+  irrecv.enableIRIn(); // 啟動接收
+  */
 }
 
+void loop() {
+  /*
+  if (irrecv.decode(&results)) { // 接收紅外線訊號並解碼
+    Serial.print("results value is "); // 輸出解碼後的資料
+    Serial.print(results.value, HEX);
+    Serial.print(", bits is ");
+    Serial.print(results.bits);
+    Serial.print(", decode_type is ");
+    Serial.println(results.decode_type);
+    irrecv.resume(); // 準備接收下一個訊號
+  }
+  */
+}
