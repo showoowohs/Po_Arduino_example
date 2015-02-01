0fa54ce5835aa967ec59633e804807bb2df5b63b
showoowohs <showoowohs@gmail.com>
2015-02-01 11:16:20 +0800

    create ./Po_Read_Joystick folder, copy from 4-2

---
+++ 2-31_Po_Read_Joystick/Po_Read_Joystick/Po_Read_Joystick.ino
@@ -0,0 +1,41 @@
+//Compatible with the Arduino IDE 1.0
+//Library version:1.1
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+void setup()
+{
+  Serial.begin(9600);
+  lcd.init();                      // initialize the lcd 
+  Serial.println("lcd.init()");
+  
+  // Print a message to the LCD.
+  lcd.backlight();
+  Serial.println("lcd.backlight()");
+  lcd.print("Hello, world!");
+}
+
+void loop()
+{
+  //清除LCD
+  lcd.clear();
+  //指定print起始位置
+  lcd.setCursor(0, 0);
+  lcd.print("Mimi!!");
+  delay(1000);
+  
+  lcd.setCursor(0, 1);
+  lcd.print("I Love You!!");
+  delay(1000);
+  for(int i = 0 ; i < 16 ; i++){
+    lcd.clear();
+    lcd.setCursor(i, 1);
+    lcd.print("I Love You!!");
+    delay(300);
+  }
+  
+}
+
+
