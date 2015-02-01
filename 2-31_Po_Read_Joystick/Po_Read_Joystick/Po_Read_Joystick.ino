b03352356c1ccdfe5dc7b0e6fe12664a18a3e083
showoowohs <showoowohs@gmail.com>
2015-02-01 11:36:26 +0800

    modify 2-31_Po_Read_Joystick/Po_Read_Joystick/Po_Read_Joystick.ino , can read Joystick value , reference: http://babelman-blog.logdown.com/posts/142700-arduino-joystick-control-server-motor

--- 2-31_Po_Read_Joystick/Po_Read_Joystick/Po_Read_Joystick.ino
+++ 2-31_Po_Read_Joystick/Po_Read_Joystick/Po_Read_Joystick.ino
@@ -1,41 +1,32 @@
-//Compatible with the Arduino IDE 1.0
-//Library version:1.1
-#include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
+int JoyStick_X = A0; //x
+int JoyStick_Y = A1; //y
+int JoyStick_SW = A2; //SW 
 
-LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
-
-void setup()
+void setup() 
 {
-  Serial.begin(9600);
-  lcd.init();                      // initialize the lcd 
-  Serial.println("lcd.init()");
-  
-  // Print a message to the LCD.
-  lcd.backlight();
-  Serial.println("lcd.backlight()");
-  lcd.print("Hello, world!");
-}
+  /*
+  pinMode(JoyStick_X, INPUT); 
+  pinMode(JoyStick_Y, INPUT); 
+  pinMode(JoyStick_SW, INPUT); 
+  */
+  Serial.begin(9600); // 9600 bps 
+} 
+
+
 
 void loop()
 {
-  //清除LCD
-  lcd.clear();
-  //指定print起始位置
-  lcd.setCursor(0, 0);
-  lcd.print("Mimi!!");
-  delay(1000);
-  
-  lcd.setCursor(0, 1);
-  lcd.print("I Love You!!");
-  delay(1000);
-  for(int i = 0 ; i < 16 ; i++){
-    lcd.clear();
-    lcd.setCursor(i, 1);
-    lcd.print("I Love You!!");
-    delay(300);
-  }
-  
+  int tmp = 0;
+  tmp = analogRead(JoyStick_X);
+  Serial.print("X:"); 
+  Serial.print(tmp, DEC); 
+  tmp = analogRead(JoyStick_Y);
+  Serial.print(" | Y:"); 
+  Serial.print(tmp, DEC); 
+  tmp = analogRead(JoyStick_SW);
+  Serial.print(" | SW: "); 
+  Serial.println(tmp, DEC); 
+  delay(100); 
 }
 
 
